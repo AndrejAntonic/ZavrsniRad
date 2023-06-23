440f867ee509602956e66143d48dc90a9c7e635d
aantonic20 <100687011+aantonic20@users.noreply.github.com>
2023-06-23 22:24:51 +0200

    Configured title to show just the title in different orderings of name and title.

--- app/src/main/java/com/example/foikadrovskanfc/helpers/ResponseHandler.kt
+++ app/src/main/java/com/example/foikadrovskanfc/helpers/ResponseHandler.kt
@@ -1,5 +1,6 @@
 package com.example.foikadrovskanfc.helpers
 
+import androidx.compose.ui.text.capitalize
 import com.example.foikadrovskanfc.entities.Personnel
 import com.example.foikadrovskanfc.entities.UserResponse
 
@@ -7,24 +8,24 @@ class ResponseHandler {
 
     fun convertResponseToPersonnelList(responseBody: UserResponse): MutableList<Personnel> {
         val personnelList: MutableList<Personnel> = mutableListOf()
+        var title: String
+        var lastIndex: Int
         for (personnel in responseBody.member) {
-            val lastIndex = personnel.nameWithTitleHr.lastIndexOf('.')
-            val personnelToAdd = if (lastIndex != -1) {
-                Personnel(
-                    personnel.id,
-                    personnel.nameWithTitleHr,
-                    //personnel.nameWithTitleHr.substring(0, lastIndex) + ".",
-                    personnel.firstName,
-                    personnel.lastName
-                )
-            } else {
-                Personnel(
-                    personnel.id,
-                    "",
-                    personnel.firstName,
-                    personnel.lastName
-                )
+            val containsComma = personnel.nameWithTitleHr.contains(",")
+            if(containsComma) {
+                lastIndex = personnel.nameWithTitleHr.lastIndexOf(',')
+                title = personnel.nameWithTitleHr.substring(lastIndex + 1)
+                title = title.removePrefix(" ")
+                title = title.replaceFirstChar { it.uppercase() }
             }
+            else{
+                lastIndex = personnel.nameWithTitleHr.lastIndexOf('.')
+                title = if(lastIndex != -1)
+                        personnel.nameWithTitleHr.substring(0, lastIndex) + "."
+                    else
+                        ""
+            }
+            val personnelToAdd = Personnel(personnel.id, title, personnel.firstName, personnel.lastName)
             personnelList.add(personnelToAdd)
         }
 

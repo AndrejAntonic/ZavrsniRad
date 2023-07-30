efd130bb95a33063c7397d15ddf994bfc234a6ac
aantonic20 <100687011+aantonic20@users.noreply.github.com>
2023-07-30 12:40:12 +0200

    Configured send button in Canvas to save the image to gallery.

--- app/src/main/java/com/example/foikadrovskanfc/CanvasActivity.kt
+++ app/src/main/java/com/example/foikadrovskanfc/CanvasActivity.kt
@@ -1,5 +1,9 @@
 package com.example.foikadrovskanfc
 
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.Paint
@@ -9,16 +13,27 @@ import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.graphics.Canvas
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Button
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.foikadrovskanfc.entities.Personnel
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.util.Date
 
 class CanvasActivity : AppCompatActivity() {
     private lateinit var backButton: ImageButton
     private lateinit var imageView: ImageView
+    private lateinit var sendButton: Button
+    private val REQUEST_WRITE_STORAGE = 112
     private val width = 400
     private val height = 300
 
@@ -43,8 +58,101 @@ class CanvasActivity : AppCompatActivity() {
         imageView.setImageBitmap(originalImageBitmap)
 
         val imageByteArray = convertBitmapToByteArray(originalImageBitmap)
+
+        sendButton = findViewById(R.id.btn_sendImage)
+        sendButton.setOnClickListener {
+            Log.d("bbbbbb", "bbbbbbb")
+            saveBitmapToGallery(this@CanvasActivity, originalImageBitmap, "MyImage")
+        }
         //logByteArray(imageByteArray)
     }
+
+    private fun checkWriteStoragePermission(activity: AppCompatActivity): Boolean {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Log.d("ccccc", "ccccccc")
+            if(ContextCompat.checkSelfPermission(activity, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), REQUEST_WRITE_STORAGE)
+                Log.d("dddddd", "ddddddddd")
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun saveBitmapToGallery(context: Context, bitmap: Bitmap, displayName: String) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val filename = "${displayName}_${Date().time}.png"
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+            }
+            val resolver = context.contentResolver
+            val imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+            try {
+                imageUri?.let {
+                    val outputStream: OutputStream? = resolver.openOutputStream(it)
+                    outputStream?.use { stream ->
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }else {
+            // For Android 9 and earlier (Legacy Storage)
+            if (checkWriteStoragePermission(this@CanvasActivity)) {
+                val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                val file = File(path, "${displayName}_${Date().time}.png")
+
+                try {
+                    val outputStream = FileOutputStream(file)
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                    outputStream.close()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    /*
+    private fun saveBitmapToGallery(context: Context, bitmap: Bitmap, displayName: String) {
+        if(checkWriteStoragePermission(this@CanvasActivity)) {
+            Log.d("aaaaa", "aaaaaaaaaaa")
+            val filename = "${displayName}_${Date().time}.png"
+            val contentValues = ContentValues().apply {
+                put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                    put(MediaStore.MediaColumns.IS_PENDING, 1)
+                }
+            }
+
+            val resolver = context.contentResolver
+            val imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+            try {
+                imageUri?.let {
+                    val outputStream: OutputStream? = resolver.openOutputStream(it)
+                    outputStream?.use { stream ->
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    contentValues.clear()
+                    contentValues.put(MediaStore.MediaColumns.IS_PENDING, 0)
+                    resolver.update(imageUri!!, contentValues, null, null)
+                }
+            }
+        }
+    }
+     */
 
     private fun changeStatusBarColor(window: Window, color: Int){
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
